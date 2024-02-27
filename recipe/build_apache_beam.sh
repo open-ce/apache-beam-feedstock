f4bdf410bbb1d5bdec9e3cbc5ed7293fc3f2ed8a
ArchanaShinde1 <archana.shinde2504@gmail.com>
2024-02-27 16:32:24 +0000

    Fix build failure on ppc

--- recipe/build_apache_beam.sh
+++ recipe/build_apache_beam.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+ARCH=`uname -p`
+if [[ "${ARCH}" == 'ppc64le' ]]; then
+    # remove -fno-plt as it causes failure with numpy installation
+    # https://github.com/numpy/numpy/issues/25436
+    export CXXFLAGS="$(echo ${CXXFLAGS} | sed -e 's/ -fno-plt//')"
+    export CFLAGS="$(echo ${CFLAGS} | sed -e 's/ -fno-plt//')"
+fi
+
 $PYTHON -m pip install --no-deps --ignore-installed . --verbose
